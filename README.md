22f8796776e28b0713c6e537a83f6e0c4604f443
Irene González Fernández <igonzf06@estudiantes.unileon.es>
2024-02-15 23:14:08 +0100

    fix is_ray_bbox_intersection

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # pointing_ros
+
+## Installation
+
+```shell
+$ cd ~/ros2_ws/src
+$ git clone --recurse-submodules https://github.com/igonzf/pointing_ros.git
+$ pip3 install -r pointing_ros/yolov8_ros/requirements.txt
+$ cd ~/ros2_ws
+$ rosdep install --from-paths src --ignore-src -r -y
+$ colcon build
+```
+
+## Usage 
+
+```shell
+$ ros2 launch pointing_bringup pointing.launch.py
+```
---
+++ pointing_ros/pointing_ros/pointing_node.py
@@ -0,0 +1,154 @@
+
+import math
+
+import numpy as np
+from array import *
+import rclpy
+from rclpy.qos import qos_profile_sensor_data
+from rclpy.node import Node
+from cv_bridge import CvBridge
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import CameraInfo
+from visualization_msgs.msg import MarkerArray
+from pointing_msgs.msg import Pointing
+from yolov8_msgs.msg import KeyPoint3DArray
+from yolov8_msgs.msg import DetectionArray
+from yolov8_msgs.msg import BoundingBox3D
+
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+
+class PointingNode(Node):
+
+    def __init__(self) -> None:
+        super().__init__("pointing_node")
+
+        self.right_arm_index = [6, 8, 10]
+        self.left_arm_index = [5, 7, 9]
+        self.right_arm_xyz = []
+
+        self._class_to_color = {}
+        self.cv_bridge = CvBridge()
+
+        # topics
+        self._markers_pub = self.create_publisher(MarkerArray, "markers", 10)
+        self._pointing_pub = self.create_publisher(Pointing, "pointing", 10)
+
+        self._detections_3d_sub = Subscriber(
+            self, DetectionArray, "detections_3d"
+        )
+        self._object_detections_sub = Subscriber(
+            self, DetectionArray, "object_detections_3d"
+        )
+        self._pcl_sub = Subscriber(
+            self, PointCloud2, "depth_points", qos_profile= qos_profile_sensor_data
+        )
+        self._cam_info_sub = Subscriber(
+            self, CameraInfo, "camera_info", qos_profile= qos_profile_sensor_data
+        )
+        
+        tss = ApproximateTimeSynchronizer([ self._object_detections_sub, self._detections_3d_sub], 30, 0.1)
+        tss.registerCallback(self.pointing_cb)
+
+    def get_right_arm(self, keypoint_array) -> KeyPoint3DArray:
+        right_arm = []
+        kpts_right_arm = [i for i in keypoint_array if i.id in self.right_arm_index]
+
+        if len(kpts_right_arm)>2:            
+            right_arm = [i for i in kpts_right_arm if i.point.x != 0 and i.point.y != 0]
+
+        return right_arm
+    
+    def is_arm_stretched(self, arm_xyz, angle_thresh=30.0) -> bool:
+        p0, p1, p2 = arm_xyz[0], arm_xyz[1], arm_xyz[2]
+        vec1 = np.array(p1 - p0)
+        vec2 = np.array(p2 - p1)
+        angle = np.arccos(
+            vec1.dot(vec2)/(np.linalg.norm(vec1) * np.linalg.norm(vec2)))
+        angle = angle / math.pi * 180.0
+        is_stretched = np.abs(angle) <= angle_thresh
+        return is_stretched
+    
+    
+    def is_ray_bbox_intersection(self, p1, p2, bbox) -> bool:
+        ''' Check if the 3D line defined by two points intersects a 3D bounding box.
+            Arguments:
+                p1 {geometry_msgs/Point}: First point defining the line.
+                p2 {geometry_msgs/Point}: Second point defining the line.
+                bbox {yolov8_msgs/BoundingBox3D}: Bounding box.
+            Return:
+                intersection {bool}:
+                    True if the line intersects the bounding box, False otherwise.
+        '''
+        # Extract coordinates of keypoints
+        p1_coords = np.array([p1.x, p1.y, p1.z])
+        p2_coords = np.array([p2.x, p2.y, p2.z])
+        
+        # Calculate direction vector of the line
+        direction = p2_coords - p1_coords
+        direction /= np.linalg.norm(direction)
+        
+        # Extract center and size of bounding box
+        center = bbox.center.position
+        size = bbox.size
+        
+        # Calculate half size
+        half_size = np.array([size.x / 2.0, size.y / 2.0, size.z / 2.0])
+        
+        # Calculate min and max corners of the bounding box
+        min_corner = np.array([center.x, center.y, center.z]) - half_size
+        max_corner = np.array([center.x, center.y, center.z]) + half_size
+        
+        # Calculate distances to each face of the bounding box
+        t_min = (min_corner - p1_coords) / direction
+        t_max = (max_corner - p1_coords) / direction
+        
+        # Use broadcasting to find the minimum and maximum of t values
+        t_enter = np.min(np.maximum(t_min, t_max))
+        t_exit = np.max(np.minimum(t_min, t_max))
+        
+        # Check if the line intersects the bounding box
+        return t_enter < t_exit
+    
+
+    def pointing_cb(self, msg_object_detections: DetectionArray, msg_detections_3d: DetectionArray) -> None:
+
+        if msg_detections_3d.detections:
+            human_detections = msg_detections_3d.detections
+
+            for human_pose in human_detections:
+                pointing_msg = Pointing()
+
+                right_arm = self.get_right_arm(human_pose.keypoints3d.data)
+
+                # Check if arm exists
+                if len(right_arm) == 3:
+
+                    right_arm_xyz = []
+                    for k in right_arm:
+                        right_arm_xyz.append([k.point.x, k.point.y, k.point.z])
+
+                    is_pointing = self.is_arm_stretched(np.array(right_arm_xyz))
+
+                    # Check if the person is pointing
+                    if is_pointing:
+                        self.get_logger().info("PERSON IS POINTING")
+
+                        pointing_msg.src.id = int(human_pose.id)
+                        pointing_msg.src = human_pose.keypoints3d.data[0]
+                        
+                        # Check if the trajectory of the arm streched intersects a 3D bounding box
+                        for detection in msg_object_detections.detections:
+
+                            bbox = detection.bbox3d
+                            if self.is_ray_bbox_intersection(right_arm[0].point, right_arm[2].point, bbox):
+                                self.get_logger().info(f'PERSON POINTED TO THE BAG ID: {detection.id}')
+                                pointing_msg.object = bbox
+                                self._pointing_pub.publish(pointing_msg)
+
+
+def main():
+    rclpy.init()
+    node = PointingNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
